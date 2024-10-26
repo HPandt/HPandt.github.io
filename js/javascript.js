1f965c92383e632ad3131cb7deda1fe95afa1c46
HPandt <726523@student.inholland.nl>
2024-10-26 20:04:21 +0200

    converted webpage to pages

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    var menu = document.getElementsByClassName('menu');
+    menu.classList.toggle('active');
+  }
+  
+
+  function acceptCookies() 
+  {
+    const cookieMessage = document.querySelector('.cookie-message');
+    cookieMessage.style.display = 'none';
+  }
